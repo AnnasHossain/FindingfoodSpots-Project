1e43b9043a9b12b02e1bbdf6007de5ad5a3efadc
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-06-14 18:53:30 +0200

    load things & fetch mit mounted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: []
+    };
+  },
+  async mounted() {
+    await this.loadData();
+  },
+  methods: {
+    async loadData() {
+      try {
+        const response = await fetch('/api/items');
+        if (response.ok) {
+          this.items = await response.json();
+        } else {
+          console.error('Fehler beim Laden der Daten');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
-}
+};
 </script>
