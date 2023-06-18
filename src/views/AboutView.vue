e3c5bbbbf7b873947a5a81f5032e50cc85c9c3ac
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-06-18 20:48:09 +0200

    Button added mit verlinkung

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
 
-<a href="https://www.google.de/?hl=de" target="_blank">
+<a href="http://localhost:3000/" target="_blank">
   <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Finding Foodspots</button>
 </a>
 
