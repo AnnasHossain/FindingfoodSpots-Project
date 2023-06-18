2d6c7292962185fe413803c8975fa63172462484
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-06-18 19:45:29 +0200

    load things & fetch mit mounted

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+<a href="https://www.google.de/?hl=de" target="_blank">
+  <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Active toggle button</button>
+</a>
+
   </div>
 </template>
