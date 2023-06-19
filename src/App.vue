4ccb728bea7c6519d6ff76daa6b42c0aa640416c
AnnasHossain <annas.hossain@yahoo.de>
2023-06-19 11:13:03 +0200

    ergänzungen für foodspot page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/about">Foodspots</router-link>
+    <router-link to="/foodspots">Foodspots</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,6 +5,45 @@
 <a href="http://localhost:3000/" target="_blank">
   <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Finding Foodspots</button>
 </a>
-
   </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            foodspots: [
+                {
+                    name: "Lokalname 1",
+                    category: "Kategorie 1",
+                    address: "Adresse 1",
+                    rating: "Bewertung 1",
+                    website: "https://example.com/foodspot1"
+                },
+                {
+                    name: "Lokalname 2",
+                    category: "Kategorie 2",
+                    address: "Adresse 2",
+                    rating: "Bewertung 2",
+                    website: "https://example.com/foodspot2"
+                },
+                // Füge hier weitere Foodspots hinzu...
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+table {
+    border-collapse: collapse;
+}
+
+table, th, td {
+    border: 1px solid black;
+}
+
+th, td {
+    padding: 5px;
+}
+</style>
