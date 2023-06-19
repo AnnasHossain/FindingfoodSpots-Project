59f88c18c4c05f6ae26c87c0f2d0a59cb81d466c
AnnasHossain <annas.hossain@yahoo.de>
2023-06-19 02:29:27 +0200

    setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="home">
-    <ul>
-      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
-    </ul>
-  </div>
+    <div class="home">
+        <ul>
+            <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
-  data() {
-    return {
-      items: []
-    };
-  },
-  async mounted() {
-    await this.loadData();
-  },
-  methods: {
-    async loadData() {
-      try {
-        const response = await fetch('/api/items');
-        if (response.ok) {
-          this.items = await response.json();
-        } else {
-          console.error('Fehler beim Laden der Daten');
+    name: 'HomeView',
+    data() {
+        return {
+            items: []
+        };
+    },
+    async mounted() {
+        await this.loadData();
+    },
+    methods: {
+        async loadData() {
+            try {
+                const response = await fetch('/api/items');
+                if (response.ok) {
+                    this.items = await response.json();
+                } else {
+                    console.error('Fehler beim Laden der Daten');
+                }
+            } catch (error) {
+                console.error(error);
+            }
         }
-      } catch (error) {
-        console.error(error);
-      }
     }
-  }
 };
 </script>
