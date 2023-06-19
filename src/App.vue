22027547e5043e63fd8710666fca9042e2dc06d1
AnnasHossain <annas.hossain@yahoo.de>
2023-06-19 02:42:40 +0200

    ergänzungen für foodspot page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/about">Foodspots</router-link>
   </nav>
   <router-view/>
 </template>
@@ -28,3 +29,6 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script setup>
+import FoodspotsView from "@/views/FoodspotsView.vue";
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Foodspots',
+    name: 'Foodspots',
+    component: () => import(/* webpackChunkName: "Foodspots" */ '../views/FoodspotsView.vue')
+
   }
 ]
 
