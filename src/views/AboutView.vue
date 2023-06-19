9a131f4d11f168fdc87f1da9c8df5a63bcd0e3e1
AnnasHossain <annas.hossain@yahoo.de>
2023-06-19 11:51:17 +0200

    TABELLEN für foodspot page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="about">
+
     <h1>This is an about page</h1>
 
 <a href="http://localhost:3000/" target="_blank">
   <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Finding Foodspots</button>
 </a>
+
+      <h2>Foodspots in Berlin</h2>
+
+  <div class="col" v-for="foodspot in foodspots" :key="foodspot.id">
+
+
+      <table>
+          <thead>
+          <tr>
+              <th>Lokalname</th>
+              <th>Kategorie</th>
+              <th>Adresse</th>
+              <th>Bewertung</th>
+              <th></th>
+          </tr>
+          </thead>
+          <tbody>
+              <td>{{ foodspot.name }}</td>
+              <td>{{ foodspot.category }}</td>
+              <td>{{ foodspot.address }}</td>
+              <td>{{ foodspot.rating }}</td>
+              <td><a :href="foodspot.website" target="_blank">Besuchen</a></td>
+
+          </tbody>
+      </table>
+
+  </div>
   </div>
 </template>
 
@@ -18,7 +46,7 @@ export default {
                     name: 'Lokalname 1',
                     category: "Kategorie 1",
                     address: "Adresse 1",
-                    rating: "Bewertung 1",
+                    rating: 3,
                     website: "https://example.com/foodspot1"
                 },
                 {
@@ -26,8 +54,16 @@ export default {
                     name: "Lokalname 2",
                     category: "Kategorie 2",
                     address: "Adresse 2",
-                    rating: "Bewertung 2",
+                    rating: 4,
                     website: "https://example.com/foodspot2"
+                },
+                {
+                    id: 3,
+                    name: "Lokalname 3",
+                    category: "Kategorie 3",
+                    address: "Adresse 3",
+                    rating: 4,
+                    website: "https://example.com/foodspot3"
                 },
                 // Füge hier weitere Foodspots hinzu...
             ]
