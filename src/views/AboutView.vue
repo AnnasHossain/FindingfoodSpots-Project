53b15294c70dbd8404ca0e5cad92c35529d45706
AnnasHossain <annas.hossain@yahoo.de>
2023-06-26 16:13:59 +0200

    m4 abgabe

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="about">
-
-    <h1>This is an about page</h1>
-
-<a href="http://localhost:3000/foodspots" target="_blank">
-  <button type="button" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Finding Foodspots</button>
-</a>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <input type="text" v-model="name" placeholder="Enter username" />
+        <button type="button" class="btn btn-primary active" @click="login">Login</button>
+        <p v-if="showErrorMessage" class="error-message">Der Benutzer ist nicht vorhanden. Bitte registrieren.</p>
+    </div>
 </template>
+<script>export default {
+    data() {
+        return {
+            name: "",
+            showErrorMessage: false,
+        };
+    },
+    methods: {
+        login() {
+            fetch("http://localhost:8080/Nutzers")
+                .then(response => response.json())
+                .then(Nutzer => {
+                    const foundNutzer = Nutzer.find(Nutzers => Nutzers.name === this.name);
+
+                    if (foundNutzer) {
+                        console.log("Benutzer erfolgreich eingeloggt");
+                        this.$router.push("/foodspots");
+                    } else {
+                        this.showErrorMessage = true;
+                        console.log("Der Benutzer ist nicht vorhanden. Bitte registrieren.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Fehler beim Abrufen der Benutzerdaten:", error);
+                });
+        },
+    },
+};
+</script>
+<style>
+.error-message {
+    color: red;
+    font-weight: bold;
+}
+</style>
