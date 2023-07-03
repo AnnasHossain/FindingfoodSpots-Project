07b2a686e28591e0a1843a106ff3787f1ad07d0a
ManduulB <130990639+ManduulB@users.noreply.github.com>
2023-07-03 11:16:38 +0200

    visual Änderungen

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,137 +1,144 @@
 <template>
-    <div class="about">
-        <div class="content">
-            <h1 class="title">About Us</h1>
-            <p>Welcome to our company website. Learn more about who we are and what we do.</p>
-        </div>
-        <div class="login-form">
-            <h2 class="login-title">Log In to Our Website</h2>
-            <div class="input-group">
-                <input type="text" v-model="name" class="login-input" placeholder="Name eingeben" />
-            </div>
-            <button type="button" class="login-btn" @click="login">Log In</button>
-            <p v-if="showErrorMessage" class="error-message">The user does not exist. Please register.</p>
-            <hr class="divider" />
-            <button type="button" class="create-nutzer-btn">Create New User</button>
-        </div>
+  <div class="about">
+    <div class="content">
+      <h1 class="title">About Us</h1>
+      <p>Welcome to our company website. Learn more about who we are and what we do.</p>
     </div>
+    <div class="login-form">
+      <h2 class="login-title">Log In to Our Website</h2>
+      <div class="input-group">
+        <input type="text" v-model="name" class="login-input" placeholder="Enter Name" />
+      </div>
+      <button type="button" class="login-btn" @click="login">Log In</button>
+      <p v-if="showErrorMessage" class="error-message">The user does not exist. Please register.</p>
+      <hr class="divider" />
+      <button type="button" class="create-nutzer-btn">Create New User</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            name: "",
-            showErrorMessage: false,
-        };
-    },
-    methods: {
-        login() {
-            fetch("http://localhost:8080/Nutzers")
-                .then((response) => response.json())
-                .then((Nutzer) => {
-                    const foundNutzer = Nutzer.find((Nutzers) => Nutzers.name === this.name);
+  data() {
+    return {
+      name: "",
+      showErrorMessage: false,
+    };
+  },
+  methods: {
+    login() {
+      fetch("http://localhost:8080/Nutzers")
+          .then((response) => response.json())
+          .then((Nutzer) => {
+            const foundNutzer = Nutzer.find((Nutzers) => Nutzers.name === this.name);
 
-                    if (foundNutzer) {
-                        console.log("User logged in successfully");
-                        this.$router.push("/foodspots");
-                    } else {
-                        this.showErrorMessage = true;
-                        console.log("The user does not exist. Please register.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Failed to retrieve user data:", error);
-                });
-        },
+            if (foundNutzer) {
+              console.log("User logged in successfully");
+              this.$router.push("/foodspots");
+            } else {
+              this.showErrorMessage = true;
+              console.log("The user does not exist. Please register.");
+            }
+          })
+          .catch((error) => {
+            console.error("Failed to retrieve user data:", error);
+          });
     },
+  },
 };
 </script>
 
 <style scoped>
 .about {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #f0f2f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f2f5;
 }
 
 .content {
-    text-align: center;
-    margin-right: 30px;
+  text-align: center;
+  margin: 30px;
 }
 
 .title {
-    font-size: 28px;
-    font-weight: bold;
-    margin-bottom: 20px;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
 
 .login-form {
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  margin: 20px;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
 }
 
 .login-title {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 16px;
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 16px;
 }
 
 .input-group {
-    margin-bottom: 16px;
+  margin-bottom: 16px;
 }
 
 .login-input {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    font-size: 16px;
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  box-sizing: border-box;
 }
 
 .login-btn {
-    width: 100%;
-    padding: 10px;
-    background-color: #1877f2;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
+  width: 100%;
+  padding: 12px;
+  background-color: #1877f2;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.login-btn:hover {
+  background-color: #1659a3;
 }
 
 .error-message {
-    color: red;
-    font-size: 14px;
-    margin-top: 16px;
-}
-
-.forgot-password {
-    font-size: 14px;
-    text-decoration: none;
-    color: #1877f2;
+  color: red;
+  font-size: 14px;
+  margin-top: 16px;
 }
 
 .divider {
-    margin: 16px 0;
-    border: none;
-    border-top: 1px solid #ccc;
+  margin: 16px 0;
+  border: none;
+  border-top: 1px solid #ccc;
 }
 
-.create-account-btn {
-    width: 100%;
-    padding: 10px;
-    background-color: #42b72a;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
+.create-nutzer-btn {
+  width: 100%;
+  padding: 12px;
+  background-color: #42b72a;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.create-nutzer-btn:hover {
+  background-color: #399523;
 }
 </style>
